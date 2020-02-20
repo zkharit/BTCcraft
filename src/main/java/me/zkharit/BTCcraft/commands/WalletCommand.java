a7df48cfab59bb1cdc1d8956ea181ec17c2290db
Zane <zkharit@gmail.com>
2020-02-20 12:40:50 -0500

    command handlers
    /wallet, /sendaddress

---
+++ src/main/java/me/zkharit/BTCcraft/commands/WalletCommand.java
@@ -0,0 +1,25 @@
+package me.zkharit.BTCcraft.commands;
+
+import me.zkharit.BTCcraft.BTCcraft;
+import me.zkharit.BTCcraft.BTCcraftWallet;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class WalletCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player){
+            Player player = (Player) commandSender;
+
+
+        }
+
+        return false;
+    }
+
+    private BTCcraftWallet getWallet(Player player){
+        return null;
+    }
+}
