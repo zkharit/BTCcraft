e9755ad4cbe9a759733b51c4c04593fe358e43fc
Zane <zkharit@gmail.com>
2020-02-22 20:42:52 -0500

    Command Handlers, Server Event Handler

--- src/main/java/me/zkharit/BTCcraft/commands/WalletCommand.java
+++ src/main/java/me/zkharit/BTCcraft/commands/WalletCommand.java
@@ -2,6 +2,8 @@ package me.zkharit.BTCcraft.commands;
 
 import me.zkharit.BTCcraft.BTCcraft;
 import me.zkharit.BTCcraft.BTCcraftWallet;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,6 +14,8 @@ public class WalletCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if(commandSender instanceof Player){
             Player player = (Player) commandSender;
+
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "INFO: "+ ChatColor.AQUA + player.getName() + ChatColor.RESET + " has used /wallet");
 
 
         }
---
+++ src/main/java/me/zkharit/BTCcraft/events/ServerEvents.java
@@ -0,0 +1,15 @@
+package me.zkharit.BTCcraft.events;
+
+import me.zkharit.BTCcraft.BTCcraft;
+
+import org.bukkit.event.Listener;
+
+public class ServerEvents implements Listener {
+    BTCcraft btccraft;
+
+    public ServerEvents(BTCcraft b){
+        btccraft = b;
+    }
+
+
+}
