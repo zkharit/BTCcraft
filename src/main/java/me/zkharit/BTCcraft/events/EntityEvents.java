7b650e7013fca9d7ac3ca6962b4d18625667d4a8
Zane <zkharit@gmail.com>
2020-02-26 03:03:28 -0500

    added some player caching on player joins

--- src/main/java/me/zkharit/BTCcraft/events/EntityEvents.java
+++ src/main/java/me/zkharit/BTCcraft/events/EntityEvents.java
@@ -1,13 +1,14 @@
 package me.zkharit.BTCcraft.events;
 
 import me.zkharit.BTCcraft.BTCcraft;
-import me.zkharit.BTCcraft.BTCcraftWallet;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EntityEvents implements Listener {
     BTCcraft btccraft;
@@ -19,18 +20,27 @@ public class EntityEvents implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "BTCCRAFT INFO: "+ ChatColor.AQUA + player.getName() + ChatColor.RESET + " has joined the server");
         if(!player.hasPlayedBefore()){
             generatePlayerWallet(btccraft, player);
         }
+        btccraft.addToPlayerCache(player, player.getUniqueId());
     }
 
     private void generatePlayerWallet(BTCcraft b, Player player){
-        if(b.getGenPlayerWallets()) {
-            BTCcraftWallet wallet = b.generatePlayerWallet(player);
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "INFO:"+ ChatColor.RESET+ "generated " + player.getName() + " wallet address: " + wallet.getDespoitAddress());
+        if(b.isGenPlayerWallets()) {
+            //BTCcraftWallet wallet = b.generatePlayerWallet(player);
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "BTCCRAFT INFO: "+ ChatColor.AQUA + "generated " + player.getName() + ChatColor.RESET +" wallet address: " /*+ wallet.getDepositAddress()*/);
         }else{
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "INFO:"+ ChatColor.RESET+ "skipping player address creation disabled in config");
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "BTCCRAFT INFO: "+ ChatColor.RESET + "skipping player address creation disabled in config");
         }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+        Player player = event.getPlayer();
+        btccraft.removeFromPlayerCache(player);
+        btccraft.removeFromWalletCache(player);
     }
 
 
