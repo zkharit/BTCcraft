13a4ed2d99b69c36623e65c81d379f191fa608d5
Zane <zkharit@gmail.com>
2020-02-26 03:03:01 -0500

    retrieve balance and address /wallet

--- src/main/java/me/zkharit/BTCcraft/commands/WalletCommand.java
+++ src/main/java/me/zkharit/BTCcraft/commands/WalletCommand.java
@@ -2,6 +2,7 @@ package me.zkharit.BTCcraft.commands;
 
 import me.zkharit.BTCcraft.BTCcraft;
 import me.zkharit.BTCcraft.BTCcraftWallet;
+import org.bitcoinj.kits.WalletAppKit;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,20 +11,26 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class WalletCommand implements CommandExecutor {
+    private WalletAppKit kit;
+
+    public WalletCommand(WalletAppKit k){
+        kit = k;
+    }
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if(commandSender instanceof Player){
             Player player = (Player) commandSender;
 
             Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "INFO: "+ ChatColor.AQUA + player.getName() + ChatColor.RESET + " has used /wallet");
-
-
+            Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + "BTCCRAFT INFO: " + ChatColor.AQUA + "Address: " + ChatColor.YELLOW + kit.wallet().currentReceiveAddress().toString());
+            Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + "BTCCRAFT INFO: " + ChatColor.AQUA + "Balance: " + ChatColor.YELLOW + kit.wallet().getBalance().toFriendlyString());
         }
 
-        return false;
+        return true;
     }
 
-    private BTCcraftWallet getWallet(Player player){
+    public BTCcraftWallet getWallet(Player player){
         return null;
     }
 }
