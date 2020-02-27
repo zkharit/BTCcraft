1e2d812cdb2e05986dd74f046fe77e9f0d9765be
Zane <zkharit@gmail.com>
2020-02-27 13:58:17 -0500

    add getmnemonic so player can restore from seed if they wish

---
+++ src/main/java/me/zkharit/BTCcraft/commands/GetMnemonicCommand.java
@@ -0,0 +1,32 @@
+package me.zkharit.BTCcraft.commands;
+
+import me.zkharit.BTCcraft.BTCcraft;
+import me.zkharit.BTCcraft.BTCcraftWallet;
+import org.bitcoinj.core.Address;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GetMnemonicCommand implements CommandExecutor {
+    private BTCcraft btccraft;
+
+    public GetMnemonicCommand(BTCcraft b){
+        btccraft = b;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (strings.length != 0) {
+            return false;
+        }
+
+        if(commandSender instanceof Player){
+            Player p = (Player) commandSender;
+            p.sendMessage(ChatColor.AQUA + p.getName() + " your mnemonic to restore your BTC wallet is: " + ChatColor.YELLOW + btccraft.getBTCcrafWalletFromCache(p).getMnemonic());
+        }
+
+        return true;
+    }
+}
